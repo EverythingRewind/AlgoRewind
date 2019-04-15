dc1eea2ffa4cc46a08744464b5773c0afe34cde0
toandv <dovantoanhp@gmail.com>
2019-04-15 13:36:07 +0800

    Quicksort

--- src/main/java/algo/sorting/QuickSort.java
+++ src/main/java/algo/sorting/QuickSort.java
@@ -28,23 +28,86 @@ public class QuickSort {
             if (i < li) {
                 a[i] = less[i];
             }
-
             if (i == li) {
                 a[i] = p;
             }
-
             if (i > li) {
                 a[i] = greater[i - (li + 1)];
             }
         }
-
         return a;
+    }
+
+    public static void qsort(int[] a, int q, int r) {
+        // base case
+        if (r - q < 2) return;
+
+        // partition to a[q..p-1] and a[p+1..r]
+        // compute p
+        int p = r;
+        for (int i = q; i < r; i++) {
+            if (a[i] > a[p]) {
+                int tmp = a[i];
+                a[i] = a[p];
+                a[p] = tmp;
+                p = i;
+            }
+        }
+
+        for (int i = r - 1 ; i > 0; i--) {
+            if (a[i] > a[p]) {
+                int tmp = a[i];
+                a[i] = a[p];
+                a[p] = tmp;
+                p = i;
+            }
+        }
+
+        qsort(a, q, p -1);
+        qsort(a, p+ 1, r);
 
     }
 
+    public static void quicksort(int a[], int p, int r) {
+        // base case
+        if (r - p < 1) return;
+
+
+        // partition
+        int q = partition(a, p, r);
+
+        quicksort(a, p, q - 1);
+        quicksort(a, q + 1, r);
+    }
+
+    private static int partition(int[] a, int p, int r) {
+        int x = a[r];
+
+        // partition the array into two subarrays
+
+        // the largest index of smaller element
+        int i = p - 1;
+
+        // 4 2 1 0 5 6
+        for (int j = p; j < r; j ++) {
+            if (a[j] <= x) {
+                i++; // increase size for smaller element
+                exchange(a, i, j);
+            }
+        }
+        exchange(a, i + 1, r);
+        return i + 1;
+    }
+
+    public static void exchange(int[] a, int i, int j) {
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
     public static void main(String[] args) {
-        int[] a = {2, 1, 0, 4, 9, 100, -1, -6, -9, 100, 5,3,4,3,1};
-        qsort(a, a.length);
+        int[] a = {4, 2, 1, 0, 5, 6};
+        quicksort(a, 0 , a.length - 1);
         System.out.println(Arrays.toString(a));
     }
 
--- src/main/java/algo/sorting/QuickSort.md
+++ src/main/java/algo/sorting/QuickSort.md
@@ -9,3 +9,20 @@ The base case:  an array of 0 or 1 element
 1. Pick a pivot.
 2. Partition the array into two sub-arrays: elements less then the pivot and elements greater than the pivot.
 3. Call quick sort recursively on two sub-arrays.
+
+https://www.geeksforgeeks.org/quick-sort/
+
+CLRS:
+
+Base case: array of 1 or 1 is sorted already
+Input: A[p..r]
+
+Divide:
++ Partition/arrange the array A[p..r] into two sub-arrays A[p..q-1] and A[q+1..r].
+    ++ Each element of A[p..q-1] is less or equal to A[q]
+    ++ Each element of A[q+1.. r] is greater or equal to A[q]
++ Compute the index of p as part of the partitioning procedure
+
+Conquer:
++ Sort the tow sub-arrays A[p..q-1] and A[p+1..r] by recursive calls to quicksort
+
